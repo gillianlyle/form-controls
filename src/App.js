74c471f993499ee1f7713c714018161029b112ad
Gillian Lyle <gillian.lyle@mcdermottgroup.co.uk>
2020-02-11 08:49:00 +0000

    Input component removed

--- src/App.js
+++ src/App.js
@@ -5,10 +5,6 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      <h3>Example of Input</h3>
-      <div className="section">
-        <Input />
-      </div>
       <h3>Example of Dropdown</h3>
       <div className="section">
         <Dropdown />
