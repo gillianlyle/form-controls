7e1a347cb29ce9dc49943d8d4b3fda810a839424
Gillian Lyle <gillian.lyle@mcdermottgroup.co.uk>
2020-02-11 08:48:32 +0000

    dropdown  component updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Input, Dropdown } from './components/FormFields.jsx'
+import { Input, Dropdown } from './components/FormFields.jsx';
+import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-
-      <Input />
-
-      <br /><br />
-      <Dropdown />
+      <h3>Example of Input</h3>
+      <div className="section">
+        <Input />
+      </div>
+      <h3>Example of Dropdown</h3>
+      <div className="section">
+        <Dropdown />
+      </div>
     </div>
   );
 }
--- src/components/FormFields.jsx
+++ src/components/FormFields.jsx
@@ -1,55 +1,46 @@
 import React, {useState} from 'react';
 
-
-// Test input
-const Input = () => {
-
-    const [value, setValue] = useState('');
-    const handleChange = event => setValue(event.target.value)
-
-    return (
-        <input value={value} onChange={handleChange} />
-    )
-}
-
 // Dropdown menu
 // Each time you add a new option, a new list item should appear with that value
 // The value of each option, should populate the dropdown options
+// Allow user to delete an option
 
 const Dropdown = () => {
 
     const [value, setValue] = useState('');
     const [options, setOptions] = useState([]);
 
-    const handleChange = event => {setValue(event.target.value);}
-    const handleAdd = event => {setOptions([...options, value]);} 
+    const handleChange = event => setValue(event.target.value);
+    const handleAdd = event => setOptions([...options, value]);
+    const handleDelete = (index, option) => setOptions(options.filter(option => option !== index));
+
 
     return (
         <>
-
-        Add option to dropdown menu <br />
-       
-        <ul>
-            <li>
-                <input id="option" type="text" value={value} onChange={handleChange} placeholder="Add Option" /><button type="button" onClick={handleAdd}>Add</button></li>
-                
+        <div className="left">
+            <ul>
+                <li>
+                    <input name="addOption" type="text" value={value.addOption} onChange={handleChange} placeholder="Add Option" />
+                    <button type="button" onClick={handleAdd}>Add</button>
+                </li>
+                    
                 {options.map((option, index) => (
-                    <><li key={index}>{index + 1}. <input defaultValue={value} disabled /></li></>
+                    <><li key={index}>{index + 1}. <input defaultValue={value} disabled /><button onClick={() => handleDelete(option)}>x</button></li></>
                 ))}
-        </ul>
-
-        <br /><br />dropdown <br/>
-        <select value={value} onChange={handleChange}>
-            {options.map((option) => (
-                <option>{option}</option>
-            ))}
-        </select>
-
-
-        {console.log("value: ", value)}
+            </ul>
+        </div>
+        
+        <div>
+            <input type="text" list="options" placeholder="Select option" />
+            <datalist id="options">
+                {options.map((option) => (
+                    <option value={option} />
+                ))}
+            </datalist>
+        </div>
         </>
     )
 }
 
 
-export {Input, Dropdown};
+export {Dropdown};
