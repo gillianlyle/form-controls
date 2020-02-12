b52699416c6fb61f72c39f5f4057532e6459ba29
Gillian Lyle <gillian.lyle@mcdermottgroup.co.uk>
2020-02-12 15:05:08 +0000

    dropdown component now allows inline editing

--- src/components/FormFields.jsx
+++ src/components/FormFields.jsx
@@ -1,54 +1,79 @@
 import React, {useState} from 'react';
 
-// Dropdown menu
-// Each time you add a new option, a new list item should appear with that value
-// The value of each option, should populate the dropdown options
-// Allow user to delete an option
-
 const Dropdown = () => {
 
-    const [value, setValue] = useState('');
-    const [options, setOptions] = useState([]);
+    // initialise state
+    const [options, setOptions] = useState([{content: 'Option 1'}]);
 
-    const handleChange = event => setValue(event.target.value);
-    const handleAdd = event => setOptions([...options, value]);
-    const handleDelete = (option, index) => {
-        // Create a new array of all the options except the option we want to delete
-        const updatedList = options.filter((_, index) => index !== option);
-        setOptions(updatedList);       
-    };
+    // Pass in the input’s event and the index of the option. 
+    const handleKeyDown = (event, index) => {
+        // Detecting if the return key is pressed, if it is, we call createOptionAtIndex.
+        if (event.key === 'Enter') {
+        createOptionAtIndex(event, index);
+        }
+        // Detecting if the backscpace key is pressed, if it is, we call removeOptionAtIndex.
+        if (event.key === 'Backspace' && options[index].content === '') {
+        event.preventDefault();
+        return removeOptionAtIndex(index);
+        }
+    }
+
+    const createOptionAtIndex = (event, index) => {
+        const newOptions = [...options]; // Create a copy of array
+        newOptions.splice(index + 2, 0, {content: `Option ${index + 2}`}); // insert new empty option after the currently selected option
+        setOptions(newOptions); // update the original options array
+        setTimeout(() => {document.forms[0].elements[index + 1].focus()}, 0); // wait (0 milliseconds) after state updates then set focus to the new input
+
+    }
+
+    const updateOptionAtIndex = (event, index) => {
+        const newOptions = [...options]; // Create a copy of array
+        newOptions[index].content = event.target.value; // change the value of the content key with the value inside the event object
+        setOptions(newOptions); // update the original options array
+    }
+
+    const removeOptionAtIndex = (index) => {
+        if (index === 0 && options.length === 1) return ; // Keep remaining input visible
+        setOptions(options => options.slice(0, index).concat(options.slice(index + 1, options.length)));
+    }
 
     return (
         <>
-        <div className="left">
-            <ul>                    
+        <form className="left">
+            <ul>
                 {options.map((option, index) => (
                     <>
-                        <li key={index}>{index + 1}. 
-                            <input value={option} disabled />
-                            <button onClick={() => handleDelete(index)}><i className="material-icons">close</i></button>
-                        </li>
+                        <li>{index + 1}. 
+                        <input
+                            type="text"
+                            value={option.content}
+                            onKeyDown={event => handleKeyDown(event, index)}
+                            onChange={event => updateOptionAtIndex(event, index)}
+                            placeholder="Add Option"            
+                        />
+                        
+                        <button type="button" onClick={() => removeOptionAtIndex(index)}><i className="material-icons">close</i></button>
+                    
+                        <br />
+                       </li>
                     </>
                 ))}
-                <li>
-                    <input name="addOption" type="text" value={value.addOption} onChange={handleChange} placeholder="Add Option" />
-                    <button type="button" onClick={handleAdd}>Add</button>
-                </li>
             </ul>
-        </div>
-        
-        <div>
+        </form>
+        <div className="preview">
+        <p>Preview</p>
+        <br/>
             <input type="text" list="options" placeholder="Select option" />
             <datalist id="options">
                 {options.map((option) => (
-                    <option value={option} />
+                    <option value={option.content} />
                 ))}
             </datalist>
         </div>
-        </>
-    )
-}
 
+        </>
+    )   
+}
 
 const Checkbox = () => {
 
